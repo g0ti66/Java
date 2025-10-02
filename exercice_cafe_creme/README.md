ac0b19cbfe6d8d3386f411ec220ade19ac11bf5b
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-02 18:14:24 +0200

    Add Exercise

---
+++ exercice_cafe_creme/README.md
@@ -0,0 +1,1 @@
+# exercice_cafe_creme
---
+++ exercice_cafe_creme/src/main/java/fr/dampierre/Cafe.java
@@ -0,0 +1,55 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class Cafe {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Bonjour : Je suis l'assistant de la machine à café. Dites-moi les pièces jaunes dont vous disposez :");
+
+        // Prix du café
+        int coutCafe = 100;
+
+        // Initialisation du clavier
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Pièces de 1 cent : ");
+        int cent = clavier.nextInt();
+
+        System.out.print("Pièces de 5 cents : ");
+        int cinqCent = clavier.nextInt();
+        int cinqCentTotal = cinqCent * 5;
+
+        System.out.print("Pièces de 10 cents : ");
+        int dixCent = clavier.nextInt();
+        int dixCentTotal = dixCent * 10;
+
+        System.out.print("Pièces de 20 cents : ");
+        int vingtCent = clavier.nextInt();
+        int vingtCentTotal = vingtCent * 20;
+
+        System.out.print("Pièces de 50 cents : ");
+        int cinquanteCent = clavier.nextInt();
+        int cinquanteCentTotal = cinquanteCent * 50;
+
+        // Résultat
+        int totalPiece = cent + cinqCentTotal + dixCentTotal + vingtCentTotal + cinquanteCentTotal;
+
+        // Différence
+        int differencePieces = Math.abs(coutCafe - totalPiece);
+
+        // Résultat
+        if (coutCafe > totalPiece) {
+            System.out.println("Il vous manque " + differencePieces + " cents pour votre café.");
+        } else if (coutCafe == totalPiece) {
+            System.out.println("Vous avez tout juste pour vous payer le café.");
+        } else {
+            System.out.println("Vous pouvez vous payer le café et il vous restera " + differencePieces + " cents.");
+        }
+
+        clavier.close();
+    }
+
+}
