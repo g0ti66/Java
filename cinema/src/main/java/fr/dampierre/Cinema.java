2592daa68d94bf3bb72fb90fa2c5b20f09712bb0
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-04 20:51:15 +0200

    Add Exercise cinema

---
+++ cinema/src/main/java/fr/dampierre/Cinema.java
@@ -0,0 +1,33 @@
+package fr.dampierre;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+
+        // EXERCICE NON FINI
+        // EXERCICE NON FINI
+        // EXERCICE NON FINI
+
+        int ageEnfant = 10;
+        boolean enfantAccompagne = true;
+
+        if (ageEnfant >= 14) {
+            System.out.println("Vous pouvez rentrer dans le cinéma !");
+        } else if (ageEnfant < 14) {
+            System.out.println("Voyons voir si tu es accompagné...");
+            if (enfantAccompagne == true) {
+                System.out.println("Tu peux rentrer ! Tu es accompagné !");
+            } else {
+                System.out.println("Tu n'es pas accompagné ! Tu ne peux pas rentrer !");
+            }
+        } else {
+            System.out.println("Tu ne respectes pas les conditions pour rentrer dans le cinéma !");
+        }
+
+        // EXERCICE NON FINI
+        // EXERCICE NON FINI
+        // EXERCICE NON FINI
+
+    }
+
+}
