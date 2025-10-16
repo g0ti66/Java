1d7c2acfefafe3415749821b0e8777d4fe3b7e28
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-16 19:40:54 +0200

    Add M3M4

--- module_3/src/main/java/fr/dampierre/M3M3.java
+++ module_3/src/main/java/fr/dampierre/M3M3.java
@@ -5,9 +5,11 @@ public class M3M3 {
     public static void main(String[] args) {
 
         // Ligne
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) { // Si i = 2, on fait la boucle
             // Etoile
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) { // j = 1, il est plus petit ou égal que i, on affiche '*';
+                                           // j = 2, il est plus petit ou égal que i, on affiche '*'
+                                           // On s'arrête : **
                 System.out.print("*");
             }
             System.out.println();
