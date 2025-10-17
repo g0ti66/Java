5bcc420b562cf6bb0d36989833546948c64db226
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-17 22:35:31 +0200

    Add M3M5

---
+++ module_3/src/main/java/fr/dampierre/M3M5.java
@@ -0,0 +1,16 @@
+package fr.dampierre;
+
+public class M3M5 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) { // Initialisation d'un ligne
+            for (int j = 4; j >= i; j--) { // j = 4, supérieur à i, on fait " ", et ainsi de suite (donc espace 4 fois)
+                System.out.print(" ");
+            }
+            System.out.println(i);
+        }
+
+    }
+
+}
