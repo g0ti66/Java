a0fc773b2d51071b5533f656e4025efb84a44ac5
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-18 16:27:37 +0200

    Add M3M7

---
+++ module_3/src/main/java/fr/dampierre/M3M7.java
@@ -0,0 +1,16 @@
+package fr.dampierre;
+
+public class M3M7 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 1; k <= 3; k++) {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
