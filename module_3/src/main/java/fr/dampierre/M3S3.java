82736ae797f3f4cc5cd45922a73eeaabed703ed1
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-18 23:00:43 +0200

    Add M3S3

---
+++ module_3/src/main/java/fr/dampierre/M3S3.java
@@ -0,0 +1,13 @@
+package fr.dampierre;
+
+import java.lang.Math;
+
+public class M3S3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(Math.pow(i, 2) + " ");
+        }
+    }
+}
