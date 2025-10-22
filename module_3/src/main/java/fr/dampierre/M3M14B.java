70a8195dfd26ea14d70a0f117dec0521b9a972c2
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-23 00:39:33 +0200

    Add M3M14B

---
+++ module_3/src/main/java/fr/dampierre/M3M14B.java
@@ -0,0 +1,142 @@
+package fr.dampierre;
+
+public class M3M14B {
+
+    public static void main(String[] args) {
+
+        for (int i = 18; i <= 34; i += 2) {
+            for (int j = 1; j <= i; j++) {
+                for (int k = 18; k == i; k++) {
+                    for (int k2 = 1; k2 == j; k2++) {
+                        for (int k3 = 1; k3 <= 9; k3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int l = 20; l == i; l++) {
+                    for (int l2 = 3; l2 == j; l2++) {
+                        for (int l3 = 1; l3 <= 8; l3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int m = 22; m == i; m++) {
+                    for (int l2 = 5; l2 == j; l2++) {
+                        for (int l3 = 1; l3 <= 7; l3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int n = 24; n == i; n++) {
+                    for (int n2 = 7; n2 == j; n2++) {
+                        for (int n3 = 1; n3 <= 6; n3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int o = 26; o == i; o++) {
+                    for (int o2 = 9; o2 == j; o2++) {
+                        for (int o3 = 1; o3 <= 5; o3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int p = 28; p == i; p++) {
+                    for (int p2 = 11; p2 == j; p2++) {
+                        for (int p3 = 1; p3 <= 4; p3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int q = 30; q == i; q++) {
+                    for (int q2 = 13; q2 == j; q2++) {
+                        for (int q3 = 1; q3 <= 3; q3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int r = 32; r == i; r++) {
+                    for (int r2 = 15; r2 == j; r2++) {
+                        for (int r3 = 1; r3 <= 2; r3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int s = 34; s == i; s++) {
+                    for (int s2 = 17; s2 == j; s2++) {
+                        for (int s3 = 1; s3 == 1; s3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+
+                System.out.print("*");
+
+                for (int k4 = 18; k4 == i; k4++) {
+                    for (int k5 = 18; k5 == j; k5++) {
+                        for (int k6 = 1; k6 <= 9; k6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int l4 = 20; l4 == i; l4++) {
+                    for (int l5 = 18; l5 == j; l5++) {
+                        for (int l6 = 1; l6 <= 8; l6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int m4 = 22; m4 == i; m4++) {
+                    for (int m5 = 18; m5 == j; m5++) {
+                        for (int m6 = 1; m6 <= 7; m6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int n4 = 24; n4 == i; n4++) {
+                    for (int n5 = 18; n5 == j; n5++) {
+                        for (int n6 = 1; n6 <= 6; n6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int o4 = 26; o4 == i; o4++) {
+                    for (int o5 = 18; o5 == j; o5++) {
+                        for (int o6 = 1; o6 <= 5; o6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int p4 = 28; p4 == i; p4++) {
+                    for (int p5 = 18; p5 == j; p5++) {
+                        for (int p3 = 1; p3 <= 4; p3++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int q4 = 30; q4 == i; q4++) {
+                    for (int q5 = 18; q5 == j; q5++) {
+                        for (int q6 = 1; q6 <= 3; q6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int r4 = 32; r4 == i; r4++) {
+                    for (int r5 = 18; r5 == j; r5++) {
+                        for (int r6 = 1; r6 <= 2; r6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+                for (int s4 = 34; s4 == i; s4++) {
+                    for (int s5 = 18; s5 == j; s5++) {
+                        for (int s6 = 1; s6 == 1; s6++) {
+                            System.out.print("$");
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
