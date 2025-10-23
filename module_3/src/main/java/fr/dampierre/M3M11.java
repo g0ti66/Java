d8a764857c9306f743d8b2116baef158c0f90d11
gauthy66 <gauthy.dussart66@gmail.com>
2025-10-23 13:19:19 +0200

    Add comment to M3M11

--- module_3/src/main/java/fr/dampierre/M3M11.java
+++ module_3/src/main/java/fr/dampierre/M3M11.java
@@ -7,7 +7,14 @@ public class M3M11 {
         // 6 lignes (20/4 r 2)
         for (int i = 22; i >= 2; i -= 4) {
             for (int j = 1; j <= i; j++) {
+                // on va sur le premier emplacement de chaque ligne
                 for (int j2 = 1; j2 == j; j2++) {
+                    // Si i = 14 :
+                    // j3 = 18 ; j3 >= i (14) : On fait la boucle qui affichera deux fois /
+                    // k2 - 4 = 14
+                    // j3 = 14 ; j3 >= i (14) : On fait la boucle qui affichera deux fois /
+                    // j3 - 4 = 10
+                    // j3 = 10 ; j3 < i : On s'arrête
                     for (int j3 = 18; j3 >= i; j3 -= 4) {
                         for (int j4 = 1; j4 <= 2; j4++) {
                             System.out.print("\\");
