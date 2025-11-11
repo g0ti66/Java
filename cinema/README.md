6951c9ad16e304d09a80531468fd2cd72553773e
gauthy66 <gauthy.dussart66@gmail.com>
2025-11-11 13:14:26 +0100

    Update cinema

--- cinema/README.md
+++ cinema/README.md
@@ -6,6 +6,3 @@
 - certaines personnes sont interdites de cinéma (si si, ça existe) : celles-ci ne peuvent jamais entrer, même si elles remplissent les autres conditions.
 #### Le programme ne doit rien demander à l’utilisateur : les informations seront codées « en dur » dans le programme (on devra modifier les données correspondantes directement dans le code pour tester les différents cas). Seulement deux sorties (affichages) sont possibles : « Entrée autorisée » ou « Entrée refusée ». Vous essayerez d’utiliser des opérateurs logiques pour minimiser le nombres de conditions.
 #### Écrire un programme qui affiche le résultat demandé en fonction des informations qu’il contient.
-## La deuxième partie est composée d'une légère modification du code par rapport à la consigne
-## La troisième partie est composée d'un programme pour vérifier un couple d'identifiants/mot de passe
-## La quatrième partie est composée d'un programme pour vérifier si un prénom est valide
--- cinema/src/main/java/fr/dampierre/Cinema.java
+++ cinema/src/main/java/fr/dampierre/Cinema.java
@@ -4,36 +4,13 @@ public class Cinema {
 
     public static void main(String[] args) {
 
-        // LIRE LE README.md
-        // LIRE LE README.md
-        // LIRE LE README.md
-
-        System.out.println("PARTIE 1");
-        System.out.println("--------------------------------------------------------------------------");
-
-        int ageEnfant = 10;
-        boolean enfantAccompagne = true;
-
-        if (ageEnfant >= 14 || enfantAccompagne == true) {
-            System.out.println("Vous pouvez rentrer dans le cinéma !");
-        } else if (ageEnfant < 14 && enfantAccompagne != true) {
-            System.out.println("Vous ne pouvez pas rentrer !");
-        }
-
-        System.out.println("--------------------------------------------------------------------------");
-
-        System.out.println("\n");
-
-        System.out.println("PARTIE 2");
-        System.out.println("--------------------------------------------------------------------------");
-
         int ageEnfantDeux = 12;
         boolean enfantAccompagneDeux = true;
 
         if (ageEnfantDeux >= 14) {
             System.out.println("Vous pouvez rentrer dans le cinéma !");
         } else if (ageEnfantDeux < 14) {
-            System.out.println("Voyons voir si tu es accompagné...");
+            System.out.println("Vous n'avez pas l'âge requis mais voyons voir si tu es accompagné...");
             if (enfantAccompagneDeux == true) {
                 System.out.println("Tu peux rentrer ! Tu es accompagné !");
             } else {
@@ -42,40 +19,5 @@ public class Cinema {
         } else {
             System.out.println("Tu ne respectes pas les conditions pour rentrer dans le cinéma !");
         }
-
-        System.out.println("--------------------------------------------------------------------------");
-
-        System.out.println("\n");
-
-        System.out.println("PARTIE 3");
-        System.out.println("--------------------------------------------------------------------------");
-
-        String username = "G3t0";
-        String password = "123457a";
-
-        if (username == "G3t0" && password == "1234567a") {
-            System.err.println("Credential valide !");
-        } else {
-            System.out.println("Credential non valide !");
-        }
-
-        System.out.println("--------------------------------------------------------------------------");
-
-        System.out.println("\n");
-
-        System.out.println("PARTIE 4");
-        System.out.println("--------------------------------------------------------------------------");
-
-        String prenom = "Thomas";
-
-        if (prenom == "Bob" || prenom == "André") {
-            System.out.println("Vous respectez les conditions pour rentrer dans le cinéma !");
-        } else {
-            System.out.println("Vous ne respectez pas les conditions pour rentrer dans le cinéma !");
-        }
-
-        System.out.println("--------------------------------------------------------------------------");
-
     }
-
 }
