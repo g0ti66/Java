26c59690ab1f773a565e4f0aa36027baff25c6d0
gauthy66 <gauthy.dussart66@gmail.com>
2025-11-27 15:34:10 +0100

    Définition et appel d'un méthode simple

--- module_4/src/main/java/fr/dampierre/App.java
+++ module_4/src/main/java/fr/dampierre/App.java
@@ -2,7 +2,13 @@ package fr.dampierre;
 
 public class App {
 
+    public static void bienvenue() {
+        System.out.println("Bievenue !");
+    }
+
     public static void main(String[] args) {
+
+        bienvenue();
 
     }
 }
