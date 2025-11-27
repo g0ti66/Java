fc8fb408671c931d4e3cf31a1f923558aa4d8e9a
gauthy66 <gauthy.dussart66@gmail.com>
2025-11-27 16:28:40 +0100

    Dommages aléatoires

--- module_4/src/main/java/fr/dampierre/App.java
+++ module_4/src/main/java/fr/dampierre/App.java
@@ -13,10 +13,21 @@ public class App {
         System.out.println("Tes points de vie sont de " + pv);
     }
 
+    public static int degatSubit() {
+        int min = 50;
+        int max = 100;
+        // Nombre aléatoire généré
+        int degats = min + (int) (Math.random() * ((max - min) + 1));
+        return degats;
+    }
+
     public static void main(String[] args) {
 
         bienvenue();
         System.out.println();
-        combattant("Artus", 5, 5, 100);
+        combattant("Artus", 100, 100, 1000);
+        System.out.println();
+        int degats = degatSubit();
+        System.out.println("Tu as subi " + degats + " dégâts !");
     }
 }
