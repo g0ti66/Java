14290946e706c382491c1ced2a1cf56475a03f2a
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-23 02:17:14 +0100

    Update M3M11 and Bis

--- module_3/src/main/java/fr/dampierre/M3M11.java
+++ module_3/src/main/java/fr/dampierre/M3M11.java
@@ -4,37 +4,22 @@ public class M3M11 {
 
     public static void main(String[] args) {
 
-        // 6 lignes (20/4 r 2)
-        for (int i = 22; i >= 2; i -= 4) {
-            for (int j = 1; j <= i; j++) {
-                // on va sur le premier emplacement de chaque ligne
-                for (int j2 = 1; j2 == j; j2++) {
-                    // Si i = 14 :
-                    // j3 = 18 ; j3 >= i (14) : On fait la boucle qui affichera deux fois /
-                    // k2 - 4 = 14
-                    // j3 = 14 ; j3 >= i (14) : On fait la boucle qui affichera deux fois /
-                    // j3 - 4 = 10
-                    // j3 = 10 ; j3 < i : On s'arrête
-                    for (int j3 = 18; j3 >= i; j3 -= 4) {
-                        for (int j4 = 1; j4 <= 2; j4++) {
-                            System.out.print("\\");
-                        }
-                    }
+        for (int i = 22; i >= 2; i-=4) {
+            for (int j = 18; j >= i; j-=4) {
+                for (int j2 = 1; j2 <= 2; j2++) {
+                    System.out.print("\\");
                 }
-
-                System.out.print("!");
-
             }
             for (int k = 1; k <= i; k++) {
-                for (int k2 = 1; k2 == k; k2++) {
-                    for (int k3 = 18; k3 >= i; k3 -= 4) {
-                        for (int k4 = 1; k4 <= 2; k4++) {
-                            System.out.print("/");
-                        }
-                    }
+                System.out.print("!");
+            }
+            for (int l = 18; l >= i; l-=4) {
+                for (int l2 = 1; l2 <= 2; l2++) {
+                    System.out.print("/");
                 }
             }
             System.out.println();
         }
+        
     }
 }
--- module_3/src/main/java/fr/dampierre/M3M11B.java
+++ module_3/src/main/java/fr/dampierre/M3M11B.java
@@ -4,27 +4,18 @@ public class M3M11B {
 
     public static void main(String[] args) {
 
-        // On veut 8 ligne : 30 - 22 (-=4)
-        for (int i = 30; i >= 2; i -= 4) {
-            for (int j = 1; j <= i; j++) {
-                for (int j2 = 1; j2 == j; j2++) {
-                    for (int j3 = 26; j3 >= i; j3 -= 4) {
-                        for (int j4 = 1; j4 <= 2; j4++) {
-                            System.out.print("\\");
-                        }
-                    }
+        for (int i = 30; i >= 2; i-=4) {
+            for (int j = 26; j >= i; j-=4) {
+                for (int j2 = 1; j2 <= 2; j2++) {
+                    System.out.print("\\");
                 }
-
-                System.out.print("!");
-
             }
             for (int k = 1; k <= i; k++) {
-                for (int k2 = 1; k2 == k; k2++) {
-                    for (int k3 = 26; k3 >= i; k3 -= 4) {
-                        for (int k4 = 1; k4 <= 2; k4++) {
-                            System.out.print("/");
-                        }
-                    }
+                System.out.print("!");
+            }
+            for (int l = 26; l >= i; l-=4) {
+                for (int l2 = 1; l2 <= 2; l2++) {
+                    System.out.print("/");
                 }
             }
             System.out.println();
