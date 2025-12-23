2195b1f74f2ad2e84622fa9340af9b2e0c284f9c
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-23 21:28:07 +0100

    Update M3M13 and M3M13B

--- module_3/src/main/java/fr/dampierre/M3M13.java
+++ module_3/src/main/java/fr/dampierre/M3M13.java
@@ -4,33 +4,18 @@ public class M3M13 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 32; i += 8) {
-            for (int j = 0; j <= i; j++) {
-                for (int j2 = 0; j2 == j; j2++) {
-                    for (int j3 = 24; j3 >= i; j3 -= 8) {
-                        for (int j4 = 1; j4 <= 4; j4++) {
-                            System.out.print("/");
-                        }
-                    }
+        for (int i = 0; i <= 32; i+=8) {
+            for (int j = 32; j > i; j-=8) {
+                for (int k = 1; k <= 4; k++) {
+                    System.out.print("/");
                 }
             }
-
-            // Ne pas prednre la ligne 0 en compte
-            // On va de la ligne 8 à 32 en faisant des sauts de 8
-            // Si i = 16, on répète deux fois
-            for (int j = 8; j <= i; j += 8) {
-                for (int j2 = 1; j2 <= 8; j2++) {
-                    System.out.print("*");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
-
-            for (int j = 0; j <= i; j++) {
-                for (int j2 = 0; j2 == j; j2++) {
-                    for (int j3 = 24; j3 >= i; j3 -= 8) {
-                        for (int j4 = 1; j4 <= 4; j4++) {
-                            System.out.print("\\");
-                        }
-                    }
+            for (int j = 32; j > i; j-=8) {
+                for (int k = 1; k <= 4; k++) {
+                    System.out.print("\\");
                 }
             }
             System.out.println();
--- module_3/src/main/java/fr/dampierre/M3M13B.java
+++ module_3/src/main/java/fr/dampierre/M3M13B.java
@@ -4,30 +4,18 @@ public class M3M13B {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 48; i += 8) {
-            for (int j = 0; j <= i; j++) {
-                for (int j2 = 0; j2 == j; j2++) {
-                    for (int j3 = 40; j3 >= i; j3 -= 8) {
-                        for (int j4 = 1; j4 <= 4; j4++) {
-                            System.out.print("/");
-                        }
-                    }
+        for (int i = 0; i <= 40; i+=8) {
+            for (int j = 40; j > i; j-=8) {
+                for (int k = 1; k <= 4; k++) {
+                    System.out.print("/");
                 }
             }
-
-            for (int j = 8; j <= i; j += 8) {
-                for (int j2 = 1; j2 <= 8; j2++) {
-                    System.out.print("*");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
-
-            for (int j = 0; j <= i; j++) {
-                for (int j2 = 0; j2 == j; j2++) {
-                    for (int j3 = 40; j3 >= i; j3 -= 8) {
-                        for (int j4 = 1; j4 <= 4; j4++) {
-                            System.out.print("\\");
-                        }
-                    }
+            for (int j = 40; j > i; j-=8) {
+                for (int k = 1; k <= 4; k++) {
+                    System.out.print("\\");
                 }
             }
             System.out.println();
